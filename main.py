09f5cf8fbe5e67fcd193f000ac57c3d550234d8b
Jacob Bieker <jacob@bieker.tech>
2018-10-16 10:49:33 +0200

    Start on project

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+import sys
+import scipy.sparse as sparse
+
+
+"""
+This project has to do the following:
+
+python main.py 1234/home/wojtek/A3/evaluation/user_movie.npy
+
+Need to implement minshashing and LSH
+
+Jaccard Similarity
+
+"""
+
+
+
+if __name__ == "__main__":
+    # Run when used
+    # Get arguments
+    arguments = sys.argv
+    if len(arguments) < 3:
+        # Not long enough
+        print("Not enough arguments, input should be of form:\n python main.py seed path\\to\\user_movie.npy")
+        exit()
+    else:
+        # Set seed for random generator
+        np.random.seed(arguments[1])
+        data = np.load(arguments[2])
