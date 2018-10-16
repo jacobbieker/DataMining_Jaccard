d6889e055ac217d75509ac76408f9a967402abcc
Jacob Bieker <jacob@bieker.tech>
2018-10-16 11:50:48 +0200

    Make sparse matrix and add comments

--- main.py
+++ main.py
@@ -27,6 +27,11 @@ def minhashing(data):
     return NotImplementedError
 
 def lsh(minhashed):
+    """
+    Takes minhashed values and computes LSH for the values
+    :param minhashed:
+    :return:
+    """
     return NotImplementedError
 
 def write_file(data):
@@ -37,6 +42,24 @@ def calculate_similarity(data):
     lsh_output = lsh(minhashed_data)
 
     return NotImplementedError
+
+def convert_data(data):
+    """
+    Converts the data storage in the .npy file to a sparse matrix
+    :param data:
+    :return: Compressed Sparse Row matrix for use in minhashing
+    """
+
+    # Get unique values for user and movies
+    num_users = np.max(data[:,0])+1
+    num_movies = np.max(data[:,1])+1
+
+    matrix_values = np.ones(data.shape[0])
+
+    csr_matrix = sparse.csr_matrix((matrix_values, (data[:,0], data[:,1])), shape=(num_users, num_movies))
+
+    # Now get the ones for each element
+    return csr_matrix
 
 
 if __name__ == "__main__":
@@ -49,5 +72,6 @@ if __name__ == "__main__":
         exit()
     else:
         # Set seed for random generator
-        np.random.seed(arguments[1])
-        data = np.load(arguments[2])
+        np.random.seed(int(arguments[1]))
+        data = convert_data(np.load(arguments[2]))
+        calculate_similarity(data)
