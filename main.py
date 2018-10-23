1211c4d887a8d3b327d02d1ef0eb64e143ffbd0f
Jacob Bieker <jacob@bieker.tech>
2018-10-23 13:18:41 +0200

    Closer to final version
    Added checkpointing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import numpy as np
-import os
-import sys
 import time
 start_time = time.clock()
 import sys
@@ -8,84 +6,31 @@ import scipy.sparse as sparse
 import itertools
 
 
-"""
-This project has to do the following:
-
-python main.py 1234/home/wojtek/A3/evaluation/user_movie.npy
-
-Need to implement minshashing and LSH
-
-Jaccard Similarity
-
-csc and csr
-
-row for minhasing
-
-columns for similarity check
-
-Minhashing: shingles are the movies, signature is just finding the 50-150 movies that each person rates, saving it as 
-the row/column number for that user, so if user 1 rated movies in movies 1,4,5, his signature would be sig(1,4,5) as each
-movie is a row
-
-Those signatures are then hashed to buckets in LSH, and then determines how similar two users are based on the number of
-matching shingles -> matching buckets
-
-
-"""
-
-def minhashing(csr_matrix, num_users, num_movies):
+def minhashing(csc_matrix, num_users, num_movies):
     """
-    Minhashing has to find the first (or last) 1 in the column where a given column is a user
-
-    Minhashing takes a random number of permutations of the rows (movies) of M and determines for each of those
-    where the first 1 shows up in a set of hash values
-
-    The hash vales are represented as a column, so a new matrix M is the signature matrix where users are still the columns
-    but now the rows are the signatures, so from a (100000, 17770) matrix to a (120, 17770) matrix
-
-    Then LSH breaks the signature matrix into b bands of r rows each, and hashes those to the buckets somehow
-
-
-    :param csr_matrix:
-    :param num_users:
-    :param num_movies:
-    :return:
+    Does the minhashing on a CSC sparse matrix, returning the dense signature matrix
+    :param csc_matrix: CSC matrix with movies as rows, and users and columns
+    :param num_users: Number of users
+    :param num_movies: Number of movies
+    :return: Signature matrix with rows as signatures and columns as users
     """
 
     signature = 120
 
     signature_matrix = np.zeros((signature, num_users), dtype='int16')
-    print(signature_matrix.shape)
-    #print("CSR Shape: " + str(csr_matrix.shape))
 
     # now get the 120 permutations for the signatures
     for permutation in range(signature):
-        #print("Permutation: " + str(permutation))
         row_order = np.random.permutation(np.arange(num_movies))
-        #print(row_order.shape)
-        #print(row_order)
 
-        permuted_csr_matrix = csr_matrix[row_order, :]
+        # While CSC is not ideal for changing rows, this is faster than converting to csr and converting back
+        permuted_csc_matrix = csc_matrix[row_order, :]
 
-        # Now get the first (or last) 1 in each column
         # Number of columns should be the number of users
         for i in range(num_users):
-            # This chooses one column at a time and gets the min index for it?
-            first = permuted_csr_matrix.indices[permuted_csr_matrix.indptr[i]:permuted_csr_matrix.indptr[i + 1]].min()
+            # Gets all the ones in the column, chooses the min index from the list of all indices in a column
+            first = permuted_csc_matrix.indices[permuted_csc_matrix.indptr[i]:permuted_csc_matrix.indptr[i + 1]].min()
             signature_matrix[permutation, i] = first
-
-    # convert to csr matrix because of large number of zeros present still...
-    test_sig = sparse.csr_matrix(signature_matrix)
-
-    # Free up memory
-
-    print("Size of full Sig Matrix: " + str(sys.getsizeof(signature_matrix)))
-    print("Size of CSR Matrix: " + str(sys.getsizeof(test_sig)))
-
-    print("Num Zeros: " + str(test_sig.nnz))
-    print(signature_matrix)
-
-    #del signature_matrix
 
     return signature_matrix, signature
 
@@ -105,11 +50,13 @@ def lsh(sig_mat, signature, num_bands, sparse_matrix):
     bucket = []
 
     num_rows = int(np.floor(signature / num_bands))
+    # Make the sparse matrix dense for the jaccard similarity check
     sparse_matrix = sparse_matrix.toarray()
 
     # Go through each band
     current_row = 0
-    total_number_of_found_pairs = 0
+    unique_set = set()
+    total_ones_found = 0
     for bands in range(num_bands):
 
         # These are the one in the band, so good for csr matrix
@@ -117,115 +64,100 @@ def lsh(sig_mat, signature, num_bands, sparse_matrix):
         current_row += num_rows
 
         # Create the buckets
-        print("Band Maxes: " + str(band.max(1) + 1))
-
         indexes = np.ravel_multi_index(band, band.max(1) + 1)
         s_indexes = indexes.argsort()
         sorted_indexes = indexes[s_indexes]
 
         bucket_array = np.array(np.split(s_indexes, np.nonzero(sorted_indexes[1:] > sorted_indexes[:-1])[0] + 1))
 
-
-        # Sort buckets by their length, skip those with length < 2, and work up to larger numbers
-
+        # Remove buckets with same tuples
+        x = map(tuple, bucket_array)
+        bucket_array = set(x)
+        bucket_array = list(bucket_array)
 
         # Only get buckets with more than one user
         for position in range(len(bucket_array)):
             if len(bucket_array[position]) > 1:
                 bucket.append(bucket_array[position])
 
-        # Remove buckets with same tuples
-
-        x = map(tuple, bucket)
-        bucket = set(x)
-        bucket = list(bucket)
-
-        # Largest bucket size
-        def lengths(x):
-            if isinstance(x,list):
-                yield len(x)
-                for y in x:
-                    yield from lengths(y)
-
-        big_bucket = 0
-        for position in range(len(bucket)):
-            if len(bucket[position]) > big_bucket:
-                big_bucket = len(bucket[position])
-        print("\nLargeset Bucket: " + str(big_bucket))
-
-        # sorted by longest length buckets:
-        bucket = sorted(bucket, key=len)
-
-        # finding the unique candidate pairs with a similarity larger than 0.5 in the signature matrix
-        # note that this also counts (3,5) and (5,3) separately. This double counting
-        # is removed later on during creation of the txt file
-        unique_set = set()
+        # finding the unique candidate pairs with a similarity larger than 0.5
         for i in range(len(bucket)):
-            # generate a generator expression for the combinations of the pairs in
-            # a bucket
-            large_user_pair = set(
+            # creates a generator to go through all the combinations in a given bucket
+            user_pairs = set(
                 pair for pair in itertools.combinations(bucket[i], 2))
 
-            large_user_pair = large_user_pair.difference(unique_set)
+            user_pairs = user_pairs.difference(unique_set)
             # Count how many buckets both pairs have in common vs total number of buckets to get the answer
-            for j in large_user_pair:
+            for pair in user_pairs:
                 # Check if already in unique_set
-                if j[0] < j[1]:
-                    continue
-                else:
-                    # In the wrong order
-                    j = (j[1], j[0])
-                if j not in unique_set:
-                    # This is a much faster check of the similarity, not always accurate though
-                    sim = signature_similarity(
-                        j[0], j[1], sig_mat)
-                    if sim > 0.5:
+                if pair not in unique_set:
+                    # This is a much faster check of the similarity, not always accurate though, could also eliminate
+                    # some truly similar objects, but is much faster
+                    sim = signature_similarity(pair[0], pair[1], sig_mat)
+                    if sim > 0.25:
                         # Much more time consuming, but makes sure it is actually higher than 0.5
-                        j_sim = jaccards_similarity(j[0], j[1], sparse_matrix)
+                        j_sim = bool_jaccards_similarity(pair[0], pair[1], sparse_matrix)
                         if j_sim > 0.5:
-                            unique_set.add(j)
+                            unique_set.add(pair)
 
-                # Now write out for every 10 that are found
-                if len(unique_set) > total_number_of_found_pairs + 10:
-                    print("Writing File")
+                # Now write out as it goes
+                if len(unique_set) > total_ones_found + 10:
+                    # Write every 10 as a checkpoint
                     write_file(unique_set)
-                    total_number_of_found_pairs += 10
+                    total_ones_found = len(unique_set)
 
     # Also write it when its all done
     write_file(unique_set)
-
-    print(len(unique_set))
     return unique_set
+
 
 def write_file(unique_set):
     # write to txt file
+    written_values = 0
     unique_set = sorted(unique_set)
-    with open('results1.txt', 'w') as f:
-        f.write('\n'.join('%s,%s' % user_pair for user_pair in unique_set))
-    f.close()
+    # Now check if there are duplicates
+    with open("results.txt", "w") as f:
+        for set in unique_set:
+            if set[0] < set[1]:
+                f.write(str(set[0]) + "," + str(set[1]) + "\n")
+                written_values += 1
+            elif set[0] > set[1]:
+                if (set[1], set[0]) not in unique_set:
+                    f.write(str(set[1]) + "," + str(set[0]) + "\n")
+                    written_values += 1
+
     print('User-Pair found: ', len(unique_set))
-    return NotImplementedError
-
-def calculate_similarity(data):
-    minhashed_data = minhashing(data)
-    lsh_output = lsh(minhashed_data)
-
-    return NotImplementedError
+    print("Users printed: " + str(written_values))
 
 
 def signature_similarity(user1, user2, signature_matrix):
-    """    Calculate and return the similarity between two users (user1 and user2) with the signature matrix    """
-    similar = float(np.count_nonzero(signature_matrix[:, user1] == signature_matrix[:, user2])) \
-              / len(signature_matrix[:, user1])
-    return similar
+    """
+    Calculates the similarity in the signature matrix, directly since its much smaller
+    :param user1: The first user
+    :param user2: The second user
+    :param signature_matrix: The dense signature matrix
+    :return: The similarity score based on the signature matrix for two users
+    """
+    sim_score = float(np.count_nonzero(signature_matrix[:, user1] == signature_matrix[:, user2])) \
+                / len(signature_matrix[:, user1])
+    return sim_score
 
 
-def jaccards_similarity(user1, user2, sparse_matrix):
-    """     Calculate and return the Jaccard similarity between two users (user1 and user2) with the sparse matrix   """
-    sum_val = np.sum(sparse_matrix[:, user1] & sparse_matrix[:, user2])
-    sim_val = np.sum(sparse_matrix[:, user1] | sparse_matrix[:, user2])
-    jacard_sim = float(sum_val) / float(sim_val)
+def bool_jaccards_similarity(user1, user2, dense_matrix):
+    """
+    Calculates the Jaccard Similarity on a boolean matrix given two users
+    :param user1: First user
+    :param user2: Second User
+    :param dense_matrix: Movie x User Dense matrix to use
+    :return: The Jaccard similarity
+    """
+    # Numerator, the intersection of both users
+    sum_val = np.sum(dense_matrix[:, user1] & dense_matrix[:, user2])
+    # Denominator, the union of both users, so all the possibilities
+    sim_val = np.sum(dense_matrix[:, user1] | dense_matrix[:, user2])
+    jacard_sim = sum_val / sim_val
     return jacard_sim
+
 
 def convert_data(data):
     """
@@ -237,14 +169,12 @@ def convert_data(data):
     # Get unique values for user and movies
     num_users = np.max(data[:, 0]) + 1
     num_movies = np.max(data[:, 1]) + 1
-    print(num_users)
-    print(num_movies)
 
     matrix_values = np.ones(data.shape[0])
 
+    # Used boolean as that saves on memory and allows calculating the Jaccard similarity easier
     csr_matrix = sparse.csc_matrix((matrix_values, (data[:, 1], data[:, 0])), shape=(num_movies, num_users), dtype='b')
 
-    # Now get the ones for each element
     return csr_matrix
 
 
@@ -260,8 +190,6 @@ if __name__ == "__main__":
         # Set seed for random generator
         np.random.seed(int(arguments[1]))
         data = convert_data(np.load(arguments[2]))
-        sig_mat, signature = minhashing(data, 103703, 17770)
-        unique_set = lsh(sig_mat, signature, num_bands=19, sparse_matrix=data)
-        #calculate_similarity(data)
+        sig_mat, signature = minhashing(data, data.shape[1], data.shape[0])
+        unique_set = lsh(sig_mat, signature, num_bands=20, sparse_matrix=data)
         print("\nTime Taken: %.2s minutes" % ((time.clock() - start_time) / 60))
-
